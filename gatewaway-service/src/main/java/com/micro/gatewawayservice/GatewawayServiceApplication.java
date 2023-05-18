e6b7c6e886cc15abb0a492ad8e26c964f6dac2bb
AntonioPerez <p-aperez@neoris.com>
2023-05-18 11:23:14 +0200

    gateway

---
+++ gatewaway-service/src/main/java/com/micro/gatewawayservice/GatewawayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.gatewawayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewawayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewawayServiceApplication.class, args);
+	}
+
+}
---
+++ gatewaway-service/src/test/java/com/micro/gatewawayservice/GatewawayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.gatewawayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewawayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
